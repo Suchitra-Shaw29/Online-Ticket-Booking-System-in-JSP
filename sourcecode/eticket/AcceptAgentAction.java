e583f7c4012a24a52e16b1adc9e353f7b8c3e80c
Suchitra-Shaw29 <165810909+Suchitra-Shaw29@users.noreply.github.com>
2024-11-01 19:31:42 +0530

    Update AcceptAgentAction.java

--- sourcecode/eticket/AcceptAgentAction.java
+++ sourcecode/eticket/AcceptAgentAction.java
@@ -13,9 +13,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 /** 
- * MyEclipse Struts
- * Creation date: 11-08-2006
- * 
+
  * XDoclet definition:
  * @struts:action validate="true"
  * @struts:action-forward name="success" path="/AcceptAgentConf.jsp"
